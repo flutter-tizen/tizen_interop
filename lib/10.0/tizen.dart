c0bf7734671fa3f1c79b03ae1f7d267fb08ce04f
Seungsoo Lee <seungsoo47.lee@samsung.com>
2026-01-14 20:49:43 +0900

    Add symbol files for Tizen 10.0

--- lib/10.0/tizen.dart
+++ lib/10.0/tizen.dart
@@ -1,12 +1,21 @@
 library tizen_interop;
 
+import 'dart:ffi';
+
 import '../../src/bindings/10.0/generated_bindings.dart';
-import '../../src/bindings/10.0/generated_symbols.dart';
 import '../../src/lookup_provider.dart';
 
 export '../../src/bindings/10.0/generated_bindings.dart';
 export '../../src/extensions.dart';
 
 Tizen100Native? _tizen;
-Tizen100Native get tizen => _tizen ??=
-    Tizen100Native.fromLookup(LookupProvider(tizen100Symbols).lookup);
+final _lookupProvider = LookupProvider();
+Tizen100Native get tizen => _tizen ??= Tizen100Native.fromLookup(
+      <T extends NativeType>(String symbolName) {
+        return _lookupProvider.lookup<T>(symbolName);
+      },
+    );
+
+void registerTizenSymbolMap(Map<String, List<String>> symbolMap) {
+  _lookupProvider.registerSymbols(symbolMap);
+}
