1a5de1a6b85416550724e3cee2dbc4a0e5fd2ae9
Seungsoo Lee <seungsoo47.lee@samsung.com>
2026-03-24 13:02:59 +0900

    Add missing header for tizen 7.0 and 8.0

--- scripts/symgen_helper.sh
+++ scripts/symgen_helper.sh
@@ -107,13 +107,11 @@ for header in $file_from_entrypoints; do
     potential_file=$(find "$rootstraps" -type f -path "*/usr/include/$header" -print -quit)
     if [ -f "$potential_file" ]; then
         header_file="$potential_file"
-        echo "1.header: $header_file"
     fi
 
     # If not found in priority directories, fall back to searching by basename
     if [ ! -f "$header_file" ]; then
         header_file=$(find "$rootstraps" -type f -name "$(basename $header)" -print -quit)
-        echo "2.header: $header_file"
     fi
 
     if [ ! -f "$header_file" ]; then
@@ -126,7 +124,6 @@ for header in $file_from_entrypoints; do
         if [[ -n "${symbols[$symbol]}" ]]; then
             found_symbol_file=$(basename ${symbols[$symbol]})
             echo "$(basename $header_file) / $found_symbol_file"
-            echo "$symbol"
             add_symbol_file $found_symbol_file
             break;
         fi
